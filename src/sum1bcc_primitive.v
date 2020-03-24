4fe11a49b4d58613736e0dc0c7fa332e7723fe5c
anfbeltranva <61914924+anfbeltranva@users.noreply.github.com>
2020-03-23 19:46:13 -0500

    Update sum1bcc_primitive.v

--- src/sum1bcc_primitive.v
+++ src/sum1bcc_primitive.v
@@ -1,16 +1,16 @@
 module sum1bcc_primitive (A, B, Ci, Cout, S);
-
+// Se definen las entradas y salidas del modulo para el sumador de un bit: A, B , Carry de entrada, Carry de salida y la suma de A más B
   input  A;
   input  B;
   input  Ci;
   output Cout;
   output S;
-
+//Descipción de las conecciones o "cables" de las entradas y salidas de acuredo a la lógica combinacional con compuertas.
 
   wire a_ab;
   wire x_ab;
   wire cout_t;
-
+// Descripción de las entradas y salidas de cada una de las compuertas usadas.
   and(a_ab,A,B);
   xor(x_ab,A,B);
 
